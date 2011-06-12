3974c99fd5f38aff3a431e0ce8966a05c50970a7
davidalain89@gmail.com <davidalain89@gmail.com@1b0e69f1-ba6e-195e-63b5-81ec2184f2a2>
2011-06-12 04:52:00 +0000

    Adicionados comentários

--- src/br/com/ecomp/md/Amostra.java
+++ src/br/com/ecomp/md/Amostra.java
@@ -2,16 +2,32 @@ package br.com.ecomp.md;
 
 import util.Util;
 
+/**
+ * Amostra de série temporal
+ * 
+ * @author David Alain
+ */
+
 public class Amostra {
 	
 	public int id;
 	public double[] dados;
 	
+	/**
+	 * Cria uma amostra de uma série temporal através de um conjunto ordenado de dados contínuos
+	 * @param id Identificação da Amostra
+	 * @param dados Conjunto ordenado de valores contínuos da série temporal 
+	 */
 	public Amostra(int id, double[] dados){
 		this.id = id;
 		this.dados = dados;
 	}
 	
+	/**
+	 * Cria uma amostra de uma série temporal através de uma String que contém um conjunto ordenado de dados contínuos
+	 * @param id Identificação da Amostra
+	 * @param dados String contendo um conjunto ordenado de valores contínuos da série temporal 
+	 */
 	public Amostra(int id, String dados){
 		this.id = id;
 		this.dados = Util.converterDados(dados);
